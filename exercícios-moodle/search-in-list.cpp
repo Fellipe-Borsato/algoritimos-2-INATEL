a30d1087614db418df75c2e96daf7861958d5b15
Fellipe Borsato <fellipeborsato@gmail.com>
2026-04-29 18:51:44 -0300

    exercício: pesquisar na lista v1 (melhorar lógica)

--- exercícios-moodle/search-in-list.cpp
+++ exercícios-moodle/search-in-list.cpp
@@ -3,22 +3,37 @@
 
 using namespace std;
 
+bool encontrar(list<int> lista, int x){
+    list<int>::iterator p;
+    p = lista.begin();
+
+    for(int i = 0; i < 10; i++){
+        if(*p == x) return true;
+        p++;
+    }
+
+    return false;
+}
+
+
 int main(){
 
     list <int> lista;
-    int input;
+    int input, x;
 
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; input != 0; i++){
         cin >> input;
         lista.push_front(input);
-    }
-
-    for(int i = 0; i < 10; i++){
-        for(int i = 0; i < 4; i++){
-        cout << *lista.end() << " ";
-        lista.pop_back();
-    }
     }    
+
+    cin >> x;
+
+    if(encontrar(lista, x) == true){
+        cout << "Encontrado" << endl;
+    }
+    else{
+        cout << "Nao encontrado" << endl;
+    }
 
     return 0;
 }
