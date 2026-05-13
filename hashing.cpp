6e9fe2bd9bc280ee6bf3d867bad3ffae6f93d865
Fellipe Borsato <fellipeborsato@gmail.com>
2026-05-13 17:39:26 -0300

    inicial hash code

---
+++ hashing.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int hash_aux(int k, int m){
+    int mod = k%m;
+    int posicao = mod*k;
+}
+
+int main(){
+
+    int k, n;
+
+    cin >> k;
+    cin >> n;
+
+    
+    return 0;
+}
+
